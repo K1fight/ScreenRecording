743f4fcce9661abb627fdbc12dbb1c0625f09f4a
K1fight <92151239+K1fight@users.noreply.github.com>
2023-05-05 05:52:05 -0400

    Add files via upload

---
+++ src/main/java/org/recording/Main.java
@@ -0,0 +1,11 @@
+package org.recording;
+
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) throws AWTException {
+        ScreenRecording r = new ScreenRecording();
+        r.start();
+    }
+}
---
+++ src/main/java/org/recording/ScreenRecording.java
@@ -0,0 +1,58 @@
+package org.recording;
+
+import nu.pattern.*;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.videoio.*;
+import java.awt.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.*;
+import java.time.LocalTime;
+
+public class ScreenRecording {
+
+    static {OpenCV.loadLocally();}
+
+    String output;
+    Robot robot;
+    Rectangle screenSize;
+    Size size;
+    int fourcc;
+    VideoWriter writer;
+
+    public ScreenRecording() throws AWTException {
+        output = LocalTime.now() +".mp4";
+        robot = new Robot();
+        screenSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        size = new Size(screenSize.getWidth(),screenSize.getHeight());
+        fourcc = VideoWriter.fourcc('H','2','6','4');
+        writer = new VideoWriter(output,fourcc,30.0,size,true);
+    }
+
+    public  void start() {
+        int i = 0;
+        do {
+            BufferedImage image = robot.createScreenCapture(screenSize);
+            Mat img = bufferedImageToMat(image);
+            writer.write(img);
+            i++;
+        } while (i != 120);
+        writer.release();
+
+    }
+    public Mat bufferedImageToMat(BufferedImage bi) {
+        BufferedImage img = convertTo3ByteBGRType(bi);
+        Mat mat = new Mat(img.getHeight(), img.getWidth(), CvType.CV_8UC3);
+        byte[] data = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+        mat.put(0, 0, data);
+        return mat;
+    }
+    public BufferedImage convertTo3ByteBGRType(BufferedImage image) {
+        BufferedImage convertedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+        convertedImage.getGraphics().drawImage(image, 0, 0, null);
+        return convertedImage;
+    }
+}
